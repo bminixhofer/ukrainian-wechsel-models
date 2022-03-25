0d9b282946254bf354302408fef0871173961fc3
Benjamin Minixhofer <bminixhofer@gmail.com>
2022-03-25 23:59:44 +0000

    add more evaluation scripts

---
+++ eval/evaluate.sh
@@ -0,0 +1,3 @@
+bash evaluate_pos.sh $1
+bash evaluate_ner_languk.sh $1
+bash evaluate_ner_wikiann.sh $1
--- eval/evaluate_pos.sh
+++ eval/evaluate_pos.sh
@@ -1,4 +1,4 @@
 for seed in 1231 1232 1233 1234 1235 1236 1237 1238
 do
-    XRT_TPU_CONFIG="localservice;0;localhost:51011" python3 run_ner.py --max_seq_length=128 --pad_to_max_length --dataset_name=universal_dependencies --dataset_config_name=uk_iu --model_name_or_path=$1 --text_column_name=tokens --label_column_name=upos --output_dir=temp --overwrite_output_dir --do_train --do_eval --warmup_ratio=0.3 --per_device_train_batch_size=32 --num_train_epochs=10 --seed=$seed
+    XRT_TPU_CONFIG="localservice;0;localhost:51011" python3 run_ner.py --save_strategy=no --max_seq_length=128 --pad_to_max_length --dataset_name=universal_dependencies --dataset_config_name=uk_iu --model_name_or_path=$1 --text_column_name=tokens --label_column_name=upos --output_dir=temp --overwrite_output_dir --do_train --do_eval --warmup_ratio=0.3 --per_device_train_batch_size=32 --num_train_epochs=10 --per_device_eval_batch_size=1 --evaluation_strategy=epoch --seed=$seed
 done
